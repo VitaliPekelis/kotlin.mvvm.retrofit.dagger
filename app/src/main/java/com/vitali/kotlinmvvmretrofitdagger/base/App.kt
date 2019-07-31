55f040da6bc1551a313f08d1ec9df96d8ef68fe6
vitali <pedalka@gmail.com>
2019-07-31 19:12:02 +0300

    setup stetho debug bridge

--- app/src/main/java/com/vitali/kotlinmvvmretrofitdagger/base/App.kt
+++ app/src/main/java/com/vitali/kotlinmvvmretrofitdagger/base/App.kt
@@ -1,4 +1,5 @@
 package com.vitali.kotlinmvvmretrofitdagger.base
+import com.facebook.stetho.Stetho
 import com.vitali.kotlinmvvmretrofitdagger.BuildConfig
 import com.vitali.kotlinmvvmretrofitdagger.di.DaggerApplicationComponent
 import com.vitali.kotlinmvvmretrofitdagger.di.NetworkModule
@@ -6,6 +7,13 @@ import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 
 class App: DaggerApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if(BuildConfig.DEBUG){
+            Stetho.initializeWithDefaults(this)
+        }
+    }
 
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
--- app/src/main/java/com/vitali/kotlinmvvmretrofitdagger/di/NetworkModule.kt
+++ app/src/main/java/com/vitali/kotlinmvvmretrofitdagger/di/NetworkModule.kt
@@ -1,11 +1,14 @@
 package com.vitali.kotlinmvvmretrofitdagger.di
 
+import com.facebook.stetho.okhttp3.StethoInterceptor
+import com.vitali.kotlinmvvmretrofitdagger.BuildConfig
 import com.vitali.kotlinmvvmretrofitdagger.data.rest.MockRepoRepository
 import com.vitali.kotlinmvvmretrofitdagger.data.rest.NetworkRepoRepository
 import com.vitali.kotlinmvvmretrofitdagger.data.rest.RepoRepository
 import com.vitali.kotlinmvvmretrofitdagger.data.rest.RepoService
 import dagger.Module
 import dagger.Provides
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import javax.inject.Singleton
@@ -18,8 +21,14 @@ class NetworkModule constructor(private val isMockNetwork:Boolean) {
     @Singleton
     @Provides
     internal fun provideRetrofit(): Retrofit{
+        val clientBuilder = OkHttpClient.Builder()
+        if(BuildConfig.DEBUG){
+            clientBuilder.addNetworkInterceptor(StethoInterceptor())
+        }
+
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(clientBuilder.build())
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .build()
     }
